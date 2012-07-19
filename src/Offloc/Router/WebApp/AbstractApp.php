eda48c9f45e998b486ef55e3e0670c03c6c91f73
Beau Simensen <beau@dflydev.com>
2012-07-19 15:14:05 -0700

    Starting to flesh out an actual admin application.

--- src/Offloc/Router/WebApp/AbstractApp.php
+++ src/Offloc/Router/WebApp/AbstractApp.php
@@ -11,6 +11,23 @@
 
 namespace Offloc\Router\WebApp;
 
+use Dflydev\IdentityGenerator\DataStore\Dbal\DataStore;
+use Dflydev\IdentityGenerator\Generator\Base32CrockfordGenerator;
+use Dflydev\IdentityGenerator\Generator\RandomNumberGenerator;
+use Dflydev\IdentityGenerator\Generator\RandomStringGenerator;
+use Dflydev\IdentityGenerator\IdentityGenerator;
+use Doctrine\Common\EventManager;
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Setup;
+use Offloc\Router\Infrastructure\Persistence\Doctrine\Route\RouteRepository;
+use Offloc\Router\Infrastructure\Persistence\Doctrine\Service\ServiceRepository;
+use Offloc\Router\Infrastructure\Persistence\Doctrine\Session;
+use Offloc\Router\Domain\Model\Route\RouteFactory;
+use Offloc\Router\Domain\Model\Service\ServiceFactory;
+use Offloc\Router\Domain\Service\DflydevIdentityGeneratorService;
+use Offloc\Router\Domain\Service\SimpleRandomStringIdentityGeneratorService;
+use Offloc\Router\Domain\Service\UuidSecretGeneratorService;
 use Silex\Application;
 
 /**
@@ -38,11 +55,121 @@ abstract class AbstractApp extends Application
 
     protected function configure()
     {
-
         $app = $this;
 
         $app['offloc.router.projectRoot'] = __DIR__.'/../../../..';
 
         $this->register(new \Silex\Provider\UrlGeneratorServiceProvider);
+
+        $this->configureDoctrine();
+    }
+
+    private function configureDoctrine()
+    {
+        $app = $this;
+
+        $entityRoot = $app['offloc.router.projectRoot'].'/vendor/offloc/router/src/Offloc/Router/Infrastructure/Persistence/Doctrine';
+
+        $namespaces = array(
+            $entityRoot.'/Service' => 'Offloc\Router\Domain\Model\Service',
+            $entityRoot.'/Route' => 'Offloc\Router\Domain\Model\Route',
+        );
+
+        $app['doctrine.dbal.event_manager'] = $app->share(function() {
+            $eventManager = new EventManager;
+
+            return $eventManager;
+        });
+
+        $app['doctrine.configuration'] = $app->share(function($app) use ($namespaces) {
+            $config = Setup::createConfiguration('prod' !== $app['env']);
+            $driver = new \Doctrine\ORM\Mapping\Driver\SimplifiedXmlDriver($namespaces);
+            $config->setMetadataDriverImpl($driver);
+            $config->setAutoGenerateProxyClasses(true);
+            //$config->setSQLLogger(new \Doctrine\DBAL\Logging\EchoSQLLogger);
+
+            return $config;
+        });
+
+        $app['doctrine.dbal.connection'] = $app->share(function() use($app) {
+
+            if (!isset($app['doctrine.dbal.connection_options'])) {
+                throw new \InvalidArgumentException('The "doctrine.dbal.connection_options" parameter must be defined');
+            }
+            $configuration = $app['doctrine.configuration'];
+            $eventManager = $app['doctrine.dbal.event_manager'];
+
+            return DriverManager::getConnection($app['doctrine.dbal.connection_options'], $configuration, $eventManager);
+        });
+
+        $app['doctrine.orm.em'] = $app->share(function($app) {
+            $connection = $app['doctrine.dbal.connection'];
+            $configuration = $app['doctrine.configuration'];
+
+            return EntityManager::create($connection, $configuration);
+        });
+
+        $app['offloc.router.domain.model.session'] = $app->share(function($app) {
+            return new Session($app['doctrine.orm.em']);
+        });
+
+        $app['offloc.router.domain.model.route.routeFactory'] = $app->share(function($app) {
+            return new RouteFactory(
+                $app['offloc.router.domain.route.identityGenerator.routeId']
+            );
+        });
+        $app['offloc.router.domain.model.route.routeRepository'] = $app->share(function($app) {
+            $objectRepository = $app['doctrine.orm.em']->getRepository('Offloc\Router\Domain\Model\Route\Route');
+
+            return new RouteRepository($app['offloc.router.domain.model.session'], $objectRepository);
+        });
+
+        $app['offloc.router.domain.model.service.serviceFactory'] = $app->share(function($app) {
+            return new ServiceFactory(
+                $app['offloc.router.domain.service.identityGenerator.serviceKey'],
+                $app['offloc.router.domain.service.secretGenerator.serviceSecret']
+            );
+        });
+
+        $app['offloc.router.domain.model.service.serviceRepository'] = $app->share(function($app) {
+            $objectRepository = $app['doctrine.orm.em']->getRepository('Offloc\Router\Domain\Model\Service\Service');
+
+            return new ServiceRepository($app['offloc.router.domain.model.session'], $objectRepository);
+        });
+
+        $app['offloc.router.domain.route.identityGenerator.routeId'] = $app->share(function($app) {
+            $dataStore = new DataStore($app['doctrine.dbal.connection'], 'routeIdentity', 'id');
+            $randomNumberGenerator = new RandomNumberGenerator(32768, 1048575);
+            $generator = new Base32CrockfordGenerator($randomNumberGenerator);
+
+            $identityGenerator = new IdentityGenerator($dataStore, $generator);
+
+            return new DflydevIdentityGeneratorService($identityGenerator);
+        });
+
+        $app['offloc.router.domain.service.identityGenerator.serviceKey.length'] = 16;
+
+        $app['offloc.router.domain.service.identityGenerator.serviceKey.dataStore'] = $app->share(function($app) {
+            return new DataStore($app['doctrine.dbal.connection'], 'serviceIdentity', 'key');
+        });
+
+        $app['offloc.router.domain.service.identityGenerator.serviceKey.generator'] = $app->share(function($app) {
+            return RandomStringGenerator::createBase32Crockford(
+                $app['offloc.router.domain.service.identityGenerator.serviceKey.length']
+            );
+        });
+
+        $app['offloc.router.domain.service.identityGenerator.serviceKey'] = $app->share(function($app) {
+            $dataStore = $app['offloc.router.domain.service.identityGenerator.serviceKey.dataStore'];
+            $generator = $app['offloc.router.domain.service.identityGenerator.serviceKey.generator'];
+
+            $identityGenerator = new IdentityGenerator($dataStore, $generator);
+
+            return new DflydevIdentityGeneratorService($identityGenerator);
+        });
+
+        $app['offloc.router.domain.service.secretGenerator.serviceSecret'] = $app->share(function($app) {
+            return new UuidSecretGeneratorService;
+        });
     }
 }
--- src/Offloc/Router/WebApp/Admin.php
+++ src/Offloc/Router/WebApp/Admin.php
@@ -11,6 +11,9 @@
 
 namespace Offloc\Router\WebApp;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\InMemoryUserProvider;
+
 /**
  * Admin App
  *
@@ -22,12 +25,143 @@ class Admin extends AbstractApp
     {
         parent::configure();
 
-        $app = $this->app;
+        $app = $this;
 
-        $this->register(new \Silex\Provider\TwigServiceProvider, array(
+        $app->register(new \Silex\Provider\TwigServiceProvider, array(
             'twig.path' => array(
                 __DIR__ . '/Admin/Resources/views',
             ),
         ));
+
+        $app->register(new \Silex\Provider\SessionServiceProvider);
+        $app->register(new \Silex\Provider\SecurityServiceProvider);
+
+        $app['offloc.router.admin.users'] = $app['offloc.router.projectRoot'].'/config/admin.users.json';
+
+        $app['security.firewalls'] = array(
+            'login' => array(
+                'pattern' => '^/login$',
+                'anonymous' => true,
+            ),
+            'secured' => array(
+                'pattern' => '^.*$',
+                'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
+                'logout' => array('logout_path' => '/logout'),
+                'users' => $app->share(function($app) {
+                    $adminUsersFile = $app['offloc.router.admin.users'];
+
+                    $users = array();
+
+                    if (file_exists($adminUsersFile)) {
+                        $users = json_decode(file_get_contents($adminUsersFile), true);
+                    }
+
+                    return new InMemoryUserProvider($users);
+                }),
+            ),
+        );
+
+        $app->get('/', function() use ($app) {
+            return $app['twig']->render('home.html.twig');
+        })->bind('offloc_router_admin_home');
+
+        $app->post('/search', function(Request $request) use ($app) {
+            $routeRepository = $app['offloc.router.domain.model.route.routeRepository'];
+
+            $route = $routeRepository->find($request->get('query'));
+
+            return $app->redirect($app['url_generator']->generate(
+                'offloc_router_admin_route_detail',
+                array('routeId' => $route->id(),)
+            ));
+        })->bind('offloc_router_admin_search');
+
+        $app->get('/services', function() use ($app) {
+            return $app['twig']->render('service_list.html.twig', array(
+                'services' => $app['offloc.router.domain.model.service.serviceRepository']->findAll(),
+            ));
+        })->bind('offloc_router_admin_service_root');
+
+        $app->post('/services', function(Request $request) use ($app) {
+            $serviceFactory = $app['offloc.router.domain.model.service.serviceFactory'];
+            $serviceRepository = $app['offloc.router.domain.model.service.serviceRepository'];
+            $session = $app['offloc.router.domain.model.session'];
+
+            $service = $serviceFactory->create($request->get('name'), $request->get('url'));
+
+            $serviceRepository->store($service);
+
+            $session->flush();
+
+            return $app->redirect($app['url_generator']->generate(
+                'offloc_router_admin_service_detail',
+                array('serviceKey' => $service->key(),))
+            );
+        })->bind('offloc_router_admin_service_create');
+
+        $app->get('/services/{serviceKey}', function($serviceKey) use ($app) {
+            $serviceRepository = $app['offloc.router.domain.model.service.serviceRepository'];
+            $routeRepository = $app['offloc.router.domain.model.route.routeRepository'];
+
+            $service = $serviceRepository->find($serviceKey);
+
+            return $app['twig']->render('service_detail.html.twig', array(
+                'service' => $service,
+                'routes' => $routeRepository->findByService($service),
+            ));
+        })->bind('offloc_router_admin_service_detail');
+
+        $app->post('/services/{serviceKey}', function(Request $request, $serviceKey) use ($app) {
+            $serviceRepository = $app['offloc.router.domain.model.service.serviceRepository'];
+
+            $sevice = $serviceRepository->find($serviceKey);
+
+            $service->setName($request->get('name'));
+            $service->setUrl($request->get('url'));
+
+            return $app['twig']->render('service_detail.html.twig', array(
+                'service' => $serviceRepository->find($serviceKey),
+            ));
+        })->bind('offloc_router_admin_service_update');
+
+        $app->post('/routes', function(Request $request) use ($app) {
+            $serviceRepository = $app['offloc.router.domain.model.service.serviceRepository'];
+            $routeFactory = $app['offloc.router.domain.model.route.routeFactory'];
+            $routeRepository = $app['offloc.router.domain.model.route.routeRepository'];
+            $session = $app['offloc.router.domain.model.session'];
+
+            $route = $routeFactory->create(
+                $serviceRepository->find($request->get('serviceKey')),
+                $request->get('target'),
+                $request->get('name'),
+                $request->get('suggestion') ?: null
+            );
+
+            $routeRepository->store($route);
+
+            $session->flush();
+
+            return $app->redirect($app['url_generator']->generate(
+                'offloc_router_admin_route_detail',
+                array('routeId' => $route->id(),))
+            );
+        })->bind('offloc_router_admin_route_create');
+
+        $app->get('/routes/{routeId}', function($routeId) use ($app) {
+            $routeRepository = $app['offloc.router.domain.model.route.routeRepository'];
+
+            $route = $routeRepository->find($routeId);
+
+            return $app['twig']->render('route_detail.html.twig', array(
+                'route' => $route,
+            ));
+        })->bind('offloc_router_admin_route_detail');
+
+        $app->get('/login', function(Request $request) use ($app) {
+            return $app['twig']->render('login.html.twig', array(
+                'error'         => $app['security.last_error']($request),
+                'last_username' => $app['session']->get('_security.last_username'),
+            ));
+        })->bind('offloc_router_admin_login');
     }
 }
